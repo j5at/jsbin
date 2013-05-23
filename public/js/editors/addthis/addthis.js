95d7d049404bb027cebf826c2fe05ef0c6ee3f3b
J5 <j5@addthis.com>
2013-05-23 15:08:43 -0400

    Added AddThis specific inserts
    Insert addthis widget loader and html code snippets in jsbin

---
+++ public/js/editors/addthis/addthis.js
@@ -0,0 +1,134 @@
+//= require "../chrome/storage"
+
+var addthiselements = {
+  "<tb>": '<div class="addthis_toolbox">',
+  "</tb>": '</div>',
+  "<bc>": '<a class="addthis_button_compact"></a>',
+  "<bp>": '<a class="addthis_button_preferred"></a>'
+};
+
+var addthisblocks = [
+    {
+        "html": "<tb><bp><bp><bp><bp><bc></tb>",
+        "label": "toolbox, preferred",
+        "group": "toolboxes"
+    }
+];
+
+window.addthisblocks = addthisblocks; // expose a command line API
+window.addthiselements = addthiselements;
+
+addthisblocks.userSpecified = JSON.parse(localStorage.getItem('addthisblocks') || "[]");
+for (var i = 0; i < addthisblocks.userSpecified.length; i++) {
+  addthisblocks.push(addthisblocks.userSpecified[i]);
+}
+
+addthisblocks.add = function (widget) {
+  // Extract each script from a list (as documented) or use the default way
+  if (widget.scripts) {
+    widget.scripts.forEach(function (script) {
+      script.group = widget.text;
+      script.label = script.text;
+      this.userSpecified.push(script);
+      addthisblocks.push(script);
+    }.bind(this));
+  } else {
+    // Adding a widget according to the above schema
+    widget.group = 'Custom';
+    this.userSpecified.push(widget);
+    addthisblocks.push(widget);
+  }
+  try {
+    localStorage.setItem('addthisblocks', JSON.stringify(this.userSpecified));
+  } catch (e) {} // just in case of DOM_22 error, makes me so sad to use this :(
+  $('#addthisblocks').trigger('init');
+};
+
+addthisblocks.clear = function () {
+  addthisblocks.userSpecified = [];
+  localStorage.removeItem('addthisblocks');
+  var length = addthisblocks.length;
+  for (var i = 0; i < length; i++) {
+    if (addthisblocks[i].group === 'Custom') {
+      addthisblocks.splice(i, 1);
+      length--;
+    }
+  }
+  // force a refresh?
+  $('#addthisblocks').trigger('init');
+};
+
+var addthisloaders = [
+    {
+        "url": "//cache-local.addthis.com/cachefly/js/300/addthis_widget.js",
+        "label": "cache-local",
+        "group": "AddThis 3.0"
+    },
+    {
+        "url": "//cache-test.addthis.com/cachefly/js/300/addthis_widget.js",
+        "label": "cache-test",
+        "group": "AddThis 3.0"
+    },
+    {
+        "url": "//s7.addthis.com/js/300/addthis_widget.js",
+        "label": "s7/edge",
+        "group": "AddThis 3.0"
+    },
+    {
+        "url": "//cache-local.addthis.com/cachefly/js/250/addthis_widget.js?delayUpgrade=1",
+        "label": "cache-local",
+        "group": "AddThis 2.5"
+    },
+    {
+        "url": "//cache-test.addthis.com/cachefly/js/250/addthis_widget.js?delayUpgrade=1",
+        "label": "cache-test",
+        "group": "AddThis 2.5"
+    },
+    {
+        "url": "//s7.addthis.com/js/250/addthis_widget.js?delayUpgrade=1",
+        "label": "s7/edge",
+        "group": "AddThis 2.5"
+    }
+];
+
+window.addthisloaders = addthisloaders; // expose a command line API
+
+addthisloaders.userSpecified = JSON.parse(localStorage.getItem('addthisloaders') || "[]");
+for (var i = 0; i < addthisloaders.userSpecified.length; i++) {
+  addthisloaders.push(addthisloaders.userSpecified[i]);
+}
+
+addthisloaders.add = function (widget) {
+  // Extract each script from a list (as documented) or use the default way
+  if (widget.scripts) {
+    widget.scripts.forEach(function (script) {
+      script.group = widget.text;
+      script.label = script.text;
+      this.userSpecified.push(script);
+      addthisloaders.push(script);
+    }.bind(this));
+  } else {
+    // Adding a widget according to the above schema
+    widget.group = 'Custom';
+    this.userSpecified.push(widget);
+    addthisloaders.push(widget);
+  }
+  try {
+    localStorage.setItem('addthisloaders', JSON.stringify(this.userSpecified));
+  } catch (e) {} // just in case of DOM_22 error, makes me so sad to use this :(
+  $('#addthisloaders').trigger('init');
+};
+
+addthisloaders.clear = function () {
+  addthisloaders.userSpecified = [];
+  localStorage.removeItem('addthisloaders');
+  var length = addthisloaders.length;
+  for (var i = 0; i < length; i++) {
+    if (addthisloaders[i].group === 'Custom') {
+      addthisloaders.splice(i, 1);
+      length--;
+    }
+  }
+  // force a refresh?
+  $('#addthisloaders').trigger('init');
+};
---
+++ public/js/editors/addthis/addthismenus.js
@@ -0,0 +1,192 @@
+//= require "addthisloaders"
+
+var $loader = $('#addthisloaders'),
+    groups = {};
+
+$loader.bind('init', function () {
+  var i = 0,
+    j = 0,
+    k = 0,
+    loader = {},
+    groupOrder = [],
+    group = {},
+    groupLabel = '',
+    lcGroup = '';
+
+  // reset
+  groups = {};
+  $loader.empty();
+
+  for (i = 0; i < addthisloaders.length, loader = addthisloaders[i]; i++) {
+    groupLabel = loader.group || 'Other';
+    lcGroup = groupLabel.toLowerCase().replace(/[^a-z0-9]/ig, '');
+    if (groupOrder.indexOf(lcGroup) === -1) {
+      group = { label: groupLabel, addthisloaders: [], key: lcGroup };
+      groups[lcGroup] = group;
+      groupOrder.push(lcGroup);
+    } else {
+      group = groups[lcGroup];
+    }
+
+    group.addthisloaders.push(loader);
+  }
+
+  var html = ['<option value="none">None</option>'];
+
+  for (i = 0; i < groupOrder.length; i++) {
+    group = groups[groupOrder[i]];
+    html.push('<option value="" data-group="' + group.label + '" class="heading">-------------</option>');
+
+    for (j = 0; j < group.addthisloaders.length, loader = group.addthisloaders[j]; j++) {
+      html.push('<option value="' + group.key + ':' + j + '">' + loader.label + '</option>');
+    }
+  }
+
+  $loader.html( html.join('') );
+}).trigger('init');
+
+
+$loader.bind('change', function () {
+  if (!this.value) return;
+
+  var selected = this.value.split(':'),
+      group = groups[selected[0]],
+      loader = group.addthisloaders[selected[1]];
+
+  insertATResources(loader.url, true);
+}).on('click', function () {
+  analytics.loaderMenu();
+});
+
+
+
+
+var $block = $('#addthisblocks'),
+    groups = {};
+
+$block.bind('init', function () {
+  var i = 0,
+    j = 0,
+    k = 0,
+    block = {},
+    groupOrder = [],
+    group = {},
+    groupLabel = '',
+    lcGroup = '';
+
+  // reset
+  groups = {};
+  $block.empty();
+
+  for (i = 0; i < addthisblocks.length, block = addthisblocks[i]; i++) {
+    groupLabel = block.group || 'Other';
+    lcGroup = groupLabel.toLowerCase().replace(/[^a-z0-9]/ig, '');
+    if (groupOrder.indexOf(lcGroup) === -1) {
+      group = { label: groupLabel, addthisblocks: [], key: lcGroup };
+      groups[lcGroup] = group;
+      groupOrder.push(lcGroup);
+    } else {
+      group = groups[lcGroup];
+    }
+
+    group.addthisblocks.push(block);
+  }
+
+  var html = ['<option value="none">None</option>'];
+
+  for (i = 0; i < groupOrder.length; i++) {
+    group = groups[groupOrder[i]];
+    html.push('<option value="" data-group="' + group.label + '" class="heading">-------------</option>');
+
+    for (j = 0; j < group.addthisblocks.length, block = group.addthisblocks[j]; j++) {
+      html.push('<option value="' + group.key + ':' + j + '">' + block.label + '</option>');
+    }
+  }
+
+  $block.html( html.join('') );
+}).trigger('init');
+
+
+$block.bind('change', function () {
+  if (!this.value) return;
+
+  var selected = this.value.split(':'),
+      group = groups[selected[0]],
+      block = group.addthisblocks[selected[1]];
+
+  insertATResources(makeATBlocks(block.html), false);
+}).on('click', function () {
+  analytics.blockMenu();
+});
+
+
+// convert at shorthand elements to long-form HTML
+function makeATBlocks(html) {
+  var index = 0, chunk, pos, result = [];
+  while (pos != -1) {
+    pos = html.indexOf('>', index);
+    if (pos > 0) {
+      chunk = html.substring(index, pos + 1);
+      result.push(window.addthiselements[chunk]);
+      index = pos + 1;
+    }
+  }
+  return result.join('\n');
+}
+
+
+function insertATResources(urls, bottom) {
+  if (!$.isArray(urls)) {
+    urls = [urls];
+  }
+
+  var i = 0,
+      length = urls.length,
+      url = '',
+      code = editors.html.getCode(),
+      state = {
+        line: editors.html.editor.currentLine(),
+        character: editors.html.editor.getCursor().ch,
+        add: 0
+      },
+      html = [],
+      file = '';
+
+  for (i = 0; i < length; i++) {
+    url = urls[i];
+
+    file = url.split('/').pop();
+
+    if (file && code.indexOf(file + '"')) {
+      // attempt to lift out similar scripts
+      if (isCssFile(file)) {
+        code = code.replace(new RegExp('<link.*href=".*?/' + file + '".*?/>\n?'), '');
+      } else {
+        code = code.replace(new RegExp('<script.*src=".*?/' + file + '".*?><' + '/script>\n?'), '');
+      }
+      state.add--;
+    }
+
+    html.push('<' + 'script src="' + url + '"><' + '/script>');
+
+    state.add++;
+  }
+  
+  if (bottom) {
+    if (code.indexOf('</body>') !== -1) {
+      code = code.replace(/<\/body>/i, html.join('\n') + '\n</body>');
+    } else {
+      code += html.join('\n');
+    }
+  } else {
+    if (code.indexOf('<body>') !== -1) {
+      code = code.replace(/<body>/i, '<body>\n' + html.join('\n'));
+    } else {
+      code = html.join('\n') + code;
+    }
+  }
+
+  editors.html.setCode(code);
+  editors.html.editor.setCursor({ line: state.line + state.add, ch: state.character });
+
+}
